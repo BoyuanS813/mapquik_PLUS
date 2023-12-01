b78c14a50dd63f8cadb29aded57467a4110bb33d
BoyuanS813 <150402552+BoyuanS813@users.noreply.github.com>
2023-12-01 18:22:25 +0900

    mapquikPLUS

--- experiments/mapquikPLUS/download_dataset.sh
+++ experiments/mapquikPLUS/download_dataset.sh
@@ -5,9 +5,13 @@ for SRR in SRR11292121 SRR11292123 SRR11292120
 do 
     fasterq-dump -P -M 10000 $SRR
 done 
-cat SRR1129212* | seqtk sample -s $RANDOM 0.8 > CLR.fastq
-
+cat SRR* >> SRR.fastq
+seqtk sample -s $RANDOM SRR.fastq 0.8 > CLR.fastq
+rm SRR*
 
 #Real ONT reads for test
 wget https://s3-us-west-2.amazonaws.com/human-pangenomics/T2T/CHM13/nanopore/rel8-guppy-5.0.7/reads.fastq.gz
-gunzip -c reads.fastq.gz |seqkit seq -m 25000 |seqtk sample -s $RANDOM 3000000 > ONT.fastq
+gunzip -c reads.fastq.gz 
+seqkit seq -m 25000 -g reads.fastq > reads_25000.fastq 
+seqtk sample -s $RANDOM reads_25000.fastq 3000000 > ONT.fastq
+rm reads*
