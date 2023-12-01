5714ba403f1b2513315db1b7599771918620c4d4
BoyuanS813 <150402552+BoyuanS813@users.noreply.github.com>
2023-12-01 15:49:51 +0900

    mapquikPLUS

--- experiments/mapquikPLUS/table1.sh
+++ experiments/mapquikPLUS/table1.sh
@@ -7,14 +7,14 @@ REF=chm13.fa
 
 for ERROR in 0.95 0.90
 do
-    READS=simulated-chm13.10X."$ERROR".20kb 
+    READS=simulated-chm13.10X.$ERROR.20kb 
     time mapquikPLUS -t 10 --reference $REF1 $READS -p mapquikPLUS.$ERROR.CLR
     paftools.js mapeval mapquikPLUS.$ERROR.CLR.paf
 
     time minimap2 --secondary no -t 10 -x map-pb $REF > minimap2.$ERROR.CLR.paf
     paftools.js mapeval minimap2.$ERROR.CLR.paf
 
-    time mm2-fast --secondary no -t 10 -x map-pb $REF > mm2-fast.$ERROR.CLR.paf
+    time ./mm2-fast --secondary no -t 10 -x map-pb $REF > mm2-fast.$ERROR.CLR.paf
     paftools.js mapeval mm2-fast.$ERROR.CLR.paf
 
     time blend --secondary no -t 10 -x map-pb $REF > blend.$ERROR.CLR.paf
@@ -47,7 +47,7 @@ time mapquikPLUS -t 10 --reference $REF1 $READS -p mapquikPLUS.real.CLR
 awk '$12 == 60 {print}' mapquikPLUS.real.CLR.paf | wc -l
 time minimap2 --secondary no -t 10 -x map-pb $REF > minimap2.real.CLR.paf
 awk '$12 == 60 {print}' minimap2.real.CLR.paf | wc -l
-time mm2-fast --secondary no -t 10 -x map-pb $REF > mm2-fast.real.CLR.paf
+time ./mm2-fast --secondary no -t 10 -x map-pb $REF > mm2-fast.real.CLR.paf
 awk '$12 == 60 {print}' mm2-fast.real.CLR.paf | wc -l
 time blend --secondary no -t 10 -x map-pb $REF > blend.real.CLR.paf
 awk '$12 == 60 {print}' mm2.paf | wc -l
