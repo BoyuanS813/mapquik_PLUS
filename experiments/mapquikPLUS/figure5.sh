f1907e2657c5d362319de88862a38422d5b509b2
BoyuanS813 <150402552+BoyuanS813@users.noreply.github.com>
2023-12-01 16:26:51 +0900

    mapquikPLUS

--- experiments/mapquikPLUS/figure5.sh
+++ experiments/mapquikPLUS/figure5.sh
@@ -2,9 +2,9 @@
 
 REF1=chm13-oneline.fa
 
-for ERROR in 0.98 0.97 0.96 0.95 0.94 0.93 0.93 0.91 
+for ERROR in 0.98 0.97 0.96 0.95 0.94 0.93 0.92 0.91 
 do 
     echo "mapquikPLUS for simulated CLR reads with sequencing error rate $ERROR"
-    time mapquikPLUS --p mapquikPLUS-"$ERROR" t 11 --reference $REF1 simulated-chm13.10X."$ERROR".20kb.fa 
-    paftools.js mapeval mapquikPLUS-"$ERROR".paf
+    time mapquikPLUS --p mapquikPLUS-$ERROR t 11 --reference $REF1 simulated-chm13.10X.$ERROR.20kb.fa 
+    paftools.js mapeval mapquikPLUS-$ERROR.paf
 done 
