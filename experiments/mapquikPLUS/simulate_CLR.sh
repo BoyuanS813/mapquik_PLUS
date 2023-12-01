cf06d8db06fe1e1c15266640649df21f2e92862c
BoyuanS813 <150402552+BoyuanS813@users.noreply.github.com>
2023-12-01 16:33:43 +0900

    mapquikPLUS

--- experiments/mapquikPLUS/simulate_CLR.sh
+++ experiments/mapquikPLUS/simulate_CLR.sh
@@ -1,8 +1,8 @@
-
 # Simulate ranging-error-rate CLR reads
+
 REF=chm13.fa
 
-for ERROR in 0.98 0.97 0.96 0.95 0.94 0.93 0.92 0.90
+for ERROR in 0.98 0.97 0.96 0.95 0.94 0.93 0.92 0.91 0.90
 do 
     READS=simulated-chm13.10X.$ERROR.20kb
     pbsim $REF --model_qc ./model_qc-clr --accuracy-mean $ERROR --accuracy-sd 0 \
